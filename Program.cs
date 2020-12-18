36edef56fcc78baf4cc280d373ff661adf02ca8d
Sedat Kapanoglu <ssg@sourtimes.org>
2020-12-17 17:43:11 -0800

    default to "*" for filespec

--- Program.cs
+++ Program.cs
@@ -1,36 +1,49 @@
+// <copyright file="Program.cs" company="Sedat Kapanoglu">
+// Copyright © 2016-2020 Sedat Kapanoglu
+// SPDX-License-Identifier: MIT
+// </copyright>
+
 using System;
-using System.IO;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Diagnostics;
+using System.IO;
 
-namespace unbom
+namespace Unbom
 {
-    class Program
+    internal static class Program
     {
         private static readonly RootCommand cmd = new RootCommand("Removes UTF-8 BOM markers from text files")
         {
             new Argument<string>("path", "Path to scan"),
-            new Option<bool>(new [] { "-r", "--recurse" }, "recurse subdirectories"),
-            new Option<bool>(new [] { "-n", "--nobackup" }, "do not save a backup file"),
+            new Option<bool>(new[] { "-r", "--recurse" }, "recurse subdirectories"),
+            new Option<bool>(new[] { "-n", "--nobackup" }, "do not save a backup file"),
         };
 
-        static int Main(string[] args)
+        public static int Main(string[] args)
         {
             cmd.Handler = CommandHandler.Create<string, bool, bool>(unbom);
-            return cmd.Invoke(args);            
+            return cmd.Invoke(args);
         }
 
-        private static void unbom(string spec, bool recurse = false,
+        private static void unbom(
+            string spec,
+            bool recurse = false,
             bool nobackup = false)
         {
-            Debug.WriteLine($"spec={spec} recurse={recurse} nobackup={nobackup}")
+            Debug.WriteLine($"spec={spec} recurse={recurse} nobackup={nobackup}");
             string? path = Path.GetDirectoryName(spec);
             if (String.IsNullOrEmpty(path))
             {
                 path = ".";
             }
-            string pattern = Path.GetFileName(spec);
+
+            string? pattern = Path.GetFileName(spec);
+            if (String.IsNullOrEmpty(pattern))
+            {
+                pattern = "*";
+            }
+
             var files = Directory.EnumerateFiles(path, pattern, recurse
                 ? SearchOption.AllDirectories
                 : SearchOption.TopDirectoryOnly);
@@ -53,6 +66,7 @@ namespace unbom
                 {
                     return;
                 }
+
                 Console.Write("{0}: BOM found - removing...", fileName);
 
                 // GetTempFileName also creates the file
@@ -60,6 +74,7 @@ namespace unbom
                 using var outputStream = File.Create(tempName = tempFileName);
                 stream.CopyTo(outputStream);
             }
+
             string backupName = fileName + ".bak";
             File.Move(fileName, backupName, overwrite: true);
             File.Move(tempName, fileName);
@@ -67,6 +82,7 @@ namespace unbom
             {
                 File.Delete(backupName);
             }
+
             Console.WriteLine("done");
         }
     }
